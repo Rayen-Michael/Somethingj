95bd7bb353958b4de6587e779807785c3f579056
RayenMichael <66522696+Rayen-Michael@users.noreply.github.com>
2023-05-23 05:58:46 -0700

    Update sendRegisterOtp.js

--- src/modules/auth/controllers/register/sendRegisterOtp.js
+++ src/modules/auth/controllers/register/sendRegisterOtp.js
@@ -52,7 +52,7 @@ const sendRegisterOtp = catchAsyncError(async (req, res, next) => {
             subject: `Khod Yasta Codak: ${otp}`,
             htmlMessage: htmlMessage,
         });
-    console.log(${otp})
+    console.log(`Khod Codak Yasta: ${otp}`)
         res.status(200).json({
             success: true,
             message: ResponseMessages.OTP_SEND_SUCCESS,
