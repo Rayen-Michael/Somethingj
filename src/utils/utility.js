e4f04490dda5e3d7aceb428d62eb802ad469eed2
RayenMichael <66522696+Rayen-Michael@users.noreply.github.com>
2023-05-24 11:16:21 -0700

    Update utility.js

--- src/utils/utility.js
+++ src/utils/utility.js
@@ -38,28 +38,28 @@ utility.checkUsernameAvailable = async (uname) => {
 };
 
 utility.generateAuthToken = async (user) => {
-  // Load the private key
-  const privateKey = fs.readFileSync('./src/private_key.pem');
-  const rsaPrivateKey = {
-    key: privateKey,
-    passphrase: 'Rayen012011',
-    padding: RSA_PRIVATE_KEY,
-  };
+// Load the private key
+const privateKey = fs.readFileSync('private.pem');
+const rsaPrivateKey = {
+  key: privateKey,
+  passphrase: 'Rayen012011',
+  padding: RSA_PRIVATE_KEY,
+};
 
-  // Define the payload
-  const payload = { sub: '1234567890', name: 'John Doe' };
+// Define the payload
+const payload = { sub: '1234567890', name: 'John Doe' };
 
-  // Generate the JWT token
-  const token = jwt.sign({ id: user._id }, payload, rsaPrivateKey, { algorithm: 'RS256' });
-  const decodedData = jwt.decode(token);
+// Generate the JWT token
+const token = jwt.sign({ id: user._id }, payload, rsaPrivateKey, { algorithm: 'RS256' });
+const decodedData = jwt.decode(token);
 
-  const authToken = await models.AuthToken.create({
-    token: token,
-    user: user._id,
-    expiresAt: decodedData.exp,
-  });
-
-  console.log(token);
+const authToken = await models.AuthToken.create({
+  token: token,
+  user: user._id,
+  expiresAt: decodedData.exp
+  
+});
+  console.log(token)
   return authToken;
 }
 
